8b472e8cd57687f5079daf0d2e91493c110695cd
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-01-10 00:28:15 +0600

    header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { router } from "./Routes/Routes";
 function App() {
   return (
     <div className="App">
-        <RouterProvider router={router}></RouterProvider>
+        <RouterProvider router={router}>
+          
+        </RouterProvider>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,17 @@ const Header = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <div className="header-container">
+        <h6 className="intro">Hi, my name is </h6>
+        <h1 className="header-title">
+          Mohammad Ibrahim <br />
+          MEARN Stack Web Developer
+        </h1>
+        <p className="heading-paragraph">
+        I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.
+        </p>
+        <button type="button" class="btn btn-primary">check out my resume</button>
+      </div>
     </div>
   );
 };
