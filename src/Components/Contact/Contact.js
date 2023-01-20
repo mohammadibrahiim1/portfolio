f75efb4c26094f0d5055ed856c9b910a2c88e3c0
mohammadibrahiim <mohammadibrahiim1@gmail.com>
2023-01-20 23:42:54 +0600

    contact form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,14 +3,14 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div>
-      <section class="py-5 py-xl-6 contact" id="contact">
+      <section class="py-5 py-xl-6 contact-section" id="contact">
         <div class="container mb-5 mb-md-6">
           <div class="row justify-content-md-center">
             <div class="col-12 col-md-10 col-lg-8 col-xl-7 col-xxl-6 text-center">
               <h6 className="what-next">
-                <span className="serial-num">04.</span>what's next?
+                what's next?
               </h6>
-              <h2 class="mb-4 display-5">Get In Touch</h2>
+              <h2 class="mb-4 getinTouch">Get In Touch</h2>
               <p class="text-secondary mt-2">
                 {" "}
                 Although I’m not currently looking for any new opportunities, my
@@ -25,47 +25,6 @@ const Contact = () => {
           <div class="row justify-content-lg-center">
             <div class="col-12 col-lg-9">
               <div class="text-center shadow-sm overflow-hidden">
-                {/* <form action="#!">
-            <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
-              <div class="col-12">
-                <label for="fullname" class="form-label">Full Name <span class="text-danger">*</span></label>
-                <input type="text" class="form-control" id="fullname" value="" required/>
-              </div>
-              <div class="col-12 col-md-6">
-                <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
-                <div class="input-group">
-                  <span class="input-group-text">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
-                      <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
-                    </svg>
-                  </span>
-                  <input type="email" class="form-control" id="email" value="" required/>
-                </div>
-              </div>
-              <div class="col-12 col-md-6">
-                <label for="phone" class="form-label">Phone Number</label>
-                <div class="input-group">
-                  <span class="input-group-text">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
-                      <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z" />
-                    </svg>
-                  </span>
-                  <input type="tel" class="form-control" id="phone" value=""/>
-                </div>
-              </div>
-              <div class="col-12">
-                <label for="message" class="form-label">Message <span class="text-danger">*</span></label>
-                <textarea class="form-control" id="message" rows="3" required></textarea>
-              </div>
-              <div class="col-12">
-                <div class="d-grid">
-                  <button class="btn btn-primary btn-lg" type="submit">Submit</button>
-                </div>
-              </div>
-            </div>
-          </form> */}
-                {/* <button class="btn btn-primary contact-btn" type="submit">say hello</button> */}
-
                 {/* contact form modal */}
 
                 {/* <!-- Button trigger modal --> */}
@@ -103,50 +62,57 @@ const Contact = () => {
                         <form>
                           <div className=" input-field d-flex justify-content-between align-content-center text-start">
                             <div class="mb-3">
-                              <label
+                              {/* <label
                                 for="exampleInputEmail1"
                                 class="form-label"
                               >
                                 Name
-                              </label>
+                              </label> */}
                               <input
                                 type="text"
                                 class="form-control"
                                 id="exampleInputEmail1"
                                 aria-describedby="emailHelp"
+                                placeholder="name"
                               />
                               {/* <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> */}
                             </div>
-                            <div class="mb-3">
-                              <label
+                            <div class="mb-3 ">
+                              {/* <label
                                 for="exampleInputPassword1"
                                 class="form-label"
                               >
                                 Email
-                              </label>
+                              </label> */}
                               <input
                                 type="email"
                                 class="form-control"
                                 id="exampleInputPassword1"
+                                placeholder="email"
                               />
                             </div>
                           </div>
                           <div class="mb-3">
-                              <label
-                                for="exampleInputPassword1"
-                                class="form-label"
-                              >
-                                <textarea name="textarea" id="text" cols="49" rows="5" placeholder="write your message" className="textarea"></textarea>
-                              </label>
-                              
-                            </div>
+                            <label
+                              for="exampleInputPassword1"
+                              class="form-label"
+                            >
+                              <textarea
+                                name="textarea"
+                                id="text"
+                                cols="49"
+                                rows="5"
+                                placeholder="write your message"
+                                className="textarea"
+                              ></textarea>
+                            </label>
+                          </div>
 
                           <button type="submit" class="modal-submit-button">
-                            Submit
+                            Send
                           </button>
                         </form>
                       </div>
-
                     </div>
                   </div>
                 </div>
